e28a9ab71fde9b23d0370dc4960e9d2a0d1db599
Alex Meyer <alexwreyem@gmail.com>
2021-09-23 17:47:42 -0400

    update build.sh for new project structure

--- build.sh
+++ build.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-version=$(sed -nE "s/.*VERSION: (.*)/\1/p" FT-Night-Radian-LED-Controller.ino)
+version=$(sed -nE "s/.*VERSION: (.*)/\1/p" LEDController.ino)
 
 mkdir -p build
+mkdir -p _buildcache
 
-for file in configs/*.h; do
-  echo $file
-  configname=$(basename $file .h)
-  outname=${configname}_v${version}.hex
-  echo "--- $configname ---"
+build() {
+  if [[ ${2} == "" ]]; then
+    layoutname=$(basename ${1} .h)
+  else
+    layoutname=${2}
+  fi
+  outname=${layoutname}_v${version}.hex
+  cachedir=_buildcache/${layoutname}
 
-  ln -sf $file ./config.h
-  arduino-cli compile --fqbn arduino:avr:nano --output-dir _build
-  mv _build/FT-Night-Radian-LED-Controller.ino.hex build/$outname
-done
+  mkdir -p ${cachedir}
+
+  cp LEDController.ino ${cachedir}/${layoutname}.ino
+  cp -r LEDController.h src ${cachedir}
+  cp ${1} ${cachedir}/layout.h
+  
+  cd ${cachedir}
+
+  echo "Queueing up ${layoutname}..."
+
+  out=$(arduino-cli compile --fqbn arduino:avr:nano --output-dir _build)
+  mv _build/${layoutname}.ino.hex ../../build/${outname}
+
+  echo -e "--- ${layoutname} ---\n${out}\n\n"
+}
+
+if [[ $1 != "--all" ]]; then
+  build layout.h default
+else
+  for file in layouts/*.h; do
+    build $file &
+  done
+fi
+
+wait
+rm -rf _buildcache
